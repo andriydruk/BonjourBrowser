94e5e1fa4071d1794977bef97bd512358034d945
Andrew Druk <andriy.druk@gmail.com>
2022-12-04 02:42:02 +0200

    Dev: update target to version 33

--- app/src/main/java/com/druk/servicebrowser/BonjourApplication.java
+++ app/src/main/java/com/druk/servicebrowser/BonjourApplication.java
@@ -17,6 +17,7 @@ package com.druk.servicebrowser;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 import android.os.StrictMode;
 import android.util.Log;
 
@@ -24,6 +25,7 @@ import androidx.annotation.NonNull;
 
 import com.github.druk.rx2dnssd.Rx2Dnssd;
 import com.github.druk.rx2dnssd.Rx2DnssdBindable;
+import com.github.druk.rx2dnssd.Rx2DnssdEmbedded;
 
 public class BonjourApplication extends Application {
 
@@ -78,7 +80,14 @@ public class BonjourApplication extends Application {
     }
 
     private Rx2Dnssd createDnssd() {
-        Log.i(TAG, "Using bindable version of dns sd");
-        return new Rx2DnssdBindable(this);
+        // https://developer.android.com/about/versions/12/behavior-changes-12#mdnsresponder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Log.i(TAG, "Using embedded version of dns sd");
+            return new Rx2DnssdEmbedded(this);
+        }
+        else {
+            Log.i(TAG, "Using bindable version of dns sd");
+            return new Rx2DnssdBindable(this);
+        }
     }
 }
